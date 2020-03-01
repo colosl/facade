17148f56b0d142f47d17e48319cc339fa22f7a83
Steve Wilcox <steve.wilcox@outlook.com.au>
2020-03-01 19:27:36 +1100

    Added webpack for local development

---
+++ config/webpack.common.js
@@ -0,0 +1,49 @@
+const paths = require("./paths");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: [paths.src + "/index.js"],
+  output: {
+    path: paths.build,
+    filename: "[name].bundle.js",
+    publicPath: "/"
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: paths.static,
+        to: "assets",
+        ignore: ["*.DS_Store"]
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      title: "Webpack Boilerplate",
+      favicon: paths.src + "/images/favicon.png",
+      template: paths.src + "/index.html",
+      filename: "index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { sourceMap: true, importLoaders: 1 }
+          },
+          { loader: "postcss-loader", options: { sourceMap: true } }
+        ]
+      }
+    ]
+  }
+};
